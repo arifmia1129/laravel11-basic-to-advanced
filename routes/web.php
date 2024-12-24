b3d2a6a1dcca56a677488025c58a1c3acc3ee18c
Md. Arif <arifmia1129@gmail.com>
2024-12-24 13:00:20 +0600

    Dynamic route parameters

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return 'Welcome to learning Laravel';
 });
+
+
+Route::get('/user/{id}/{role}', function ($id, $role) {
+    return 'User id is: ' . $id . ' and role is: ' . $role;
+});
