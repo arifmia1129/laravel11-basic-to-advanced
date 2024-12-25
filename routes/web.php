f42a1734086fd46200ec1a71fe6eda8ac3c34bab
Md. Arif <arifmia1129@gmail.com>
2024-12-25 21:35:40 +0600

    Named Routes

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ Route::get('/', function () {
 
 Route::get('/about', function () {
     return 'Welcome to learning Laravel';
-});
+})->name('about.me');
 
 
 Route::get('/user/{id}/{role}', function ($id, $role) {
     return 'User id is: ' . $id . ' and role is: ' . $role;
-});
+})->name('user.details');
