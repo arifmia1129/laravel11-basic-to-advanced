fb3aa97e5c6400daf8e09e5b5a0b825a172190b5
Md. Arif <arifmia1129@gmail.com>
2024-12-26 22:46:05 +0600

    Route group

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,18 @@ Route::get('/about', function () {
 Route::get('/user/{id}/{role}', function ($id, $role) {
     return 'User id is: ' . $id . ' and role is: ' . $role;
 })->name('user.details');
+
+
+Route::group(['prefix'=> 'blog', 'as'=>'blog.'], function (){
+    Route::get('/create', function() {
+        return 'Create blog post';
+    })->name('createBlog');
+
+    Route::get('/edit/{id}', function($id) {
+        return 'Edit blog post with id: '. $id;
+    })->name('edit');
+
+    Route::get('/delete/{id}', function($id) {
+        return 'Delete blog post with id: '. $id;
+    })->name('delete');
+});
