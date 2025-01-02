5d68d2ed8612dc5d1ed8f6dd468ea1062aac7987
Md. Arif <arifmia1129@gmail.com>
2025-01-02 12:35:13 +0600

    Blade directives

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -6,8 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contact</title>
 </head>
+
+@php
+    $country = 'India';
+@endphp
+
 <body>
     <h2>Assalamu Alaikum, We are always ready to hear from you!</h2>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero possimus, odio neque itaque ipsum est? Mollitia, dolorum, fugiat facilis quia pariatur totam error ut vero dolor, maiores molestiae nemo repellendus!
+
+    <p>Please contact us at <a href="mailto:contact@example.com">contact@example.com</a></p>
+
+    @if ($country == 'Bangladesh')
+        <p>You can direct call to: +8801723112233</p>
+    @else
+        <p>Your current location is: {{ $country }}</p>
+        <p>Sorry! We have not contact number for your location! Email us.</p>
+    @endif
 </body>
 </html>
