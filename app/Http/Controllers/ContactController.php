52ba59842c2d5c50f1d5a1f17e5f2daeec845e68
Md. Arif <arifmia1129@gmail.com>
2025-01-28 10:02:02 +0600

    Custom Validation Message

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,6 +15,12 @@ class ContactController extends Controller
         $request->validate([
             'name'=> 'required|max:20|min:2',
             'email'=>'required|email'
+        ], [
+            'name.required'=> 'Name is must be required',
+            'name.min'=> 'Name character must be at least 2 characters',
+            'name.max'=> 'Name character must not be more than 20 characters',
+            'email.required'=> 'Email is must be required',
+            'email.email'=> 'Email must be a valid email format'
         ]);
 
         return response()->json($request);
